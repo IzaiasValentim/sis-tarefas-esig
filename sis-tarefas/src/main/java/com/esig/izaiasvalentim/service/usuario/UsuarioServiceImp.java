b50c93ca97ed917de1d7808c611d84c51b80eba7
IzaiasValentim <izaias451@gmail.com>
2025-02-15 15:29:16 -0300

    feature/ Adição de tratamento de erro quando há conflito de salvamento de usuário.

--- sis-tarefas/src/main/java/com/esig/izaiasvalentim/service/usuario/UsuarioServiceImp.java
+++ sis-tarefas/src/main/java/com/esig/izaiasvalentim/service/usuario/UsuarioServiceImp.java
@@ -4,7 +4,9 @@ import com.esig.izaiasvalentim.domain.entity.Usuario;
 import com.esig.izaiasvalentim.domain.repository.repositories.UsuarioRepository;
 import jakarta.ejb.EJB;
 import jakarta.ejb.Stateless;
+import jakarta.transaction.Transactional;
 
+import javax.persistence.PersistenceException;
 import java.util.List;
 
 @Stateless
@@ -13,20 +15,27 @@ public class UsuarioServiceImp {
     @EJB
     private UsuarioRepository usuarioRepository;
 
+    @Transactional
+    public Usuario salvar(Usuario usuario) {
+        Boolean usuarioExiste = usuarioRepository.buscarUsuarioPorEmail(usuario.getEmail());
+        if (usuarioExiste) {
+            throw new PersistenceException("E-mail já existente na base de dados!");
+        } else {
+            return usuarioRepository.save(usuario);
+        }
+    }
+
     public Usuario retornarPorId(Long id) {
         return usuarioRepository.findById(id);
     }
 
+    public List<Usuario> todos() {
+        return usuarioRepository.findAll();
+    }
+
+    @Transactional
     public void remover(Long idUsuario) {
         Usuario usuarioParaDeletar = retornarPorId(idUsuario);
         usuarioRepository.delete(usuarioParaDeletar);
-    }
-
-    public Usuario salvar(Usuario usuario) {
-        return usuarioRepository.save(usuario);
-    }
-
-    public List<Usuario> todos() {
-        return usuarioRepository.findAll();
     }
 }
